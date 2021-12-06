5c740ba9e7aa164d55adf5881792e88e69f6f2ea
BlueTree242 <yahiamsm2@gmail.com>
2021-12-06 23:06:38 +0200

    Ability to blacklist/hide commands

--- src/main/java/me/bluetree/advancedplhide/AdvancedPlHide.java
+++ src/main/java/me/bluetree/advancedplhide/AdvancedPlHide.java
@@ -3,6 +3,8 @@ package me.bluetree.advancedplhide;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import me.bluetree.advancedplhide.config.ConfManager;
+import me.bluetree.advancedplhide.config.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -10,20 +12,33 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AdvancedPlHide extends JavaPlugin implements Listener {
     private ProtocolManager protocolManager;
     private PacketListener listener = new PacketListener(this);
     private boolean isLegacy = false;
+    protected ConfManager<Config> confManager = ConfManager.create(getDataFolder().toPath(), "config.yml", Config.class);
+    public Config config;
     public void onLoad() {
         protocolManager = ProtocolLibrary.getProtocolManager();
     }
 
     public void onEnable() {
+        confManager.reloadConfig();
+        config = confManager.getConfigData();
         protocolManager.addPacketListener(new PacketListener(this));
         getServer().getPluginManager().registerEvents(this, this);
         String str = Bukkit.getServer().getClass().getPackage().getName();
         str = str.substring(str.lastIndexOf("v"));
         isLegacy = (str.equals("v1_8_R3") || str.contains("v1_9_R") || str.contains("v1_10_R1") || str.contains("v1_11_R1") || str.contains("v1_12_R1"));
+    }
+    public static Map<String, Group> defaultMap() {
+        Map<String, Group> map = new HashMap<>();
+        map.put("default", new Group(List.of("example1", "example2")));
+        return map;
     }
 
     public void onDisable() {
--- src/main/java/me/bluetree/advancedplhide/PacketListener.java
+++ src/main/java/me/bluetree/advancedplhide/PacketListener.java
@@ -12,6 +12,7 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.tree.CommandNode;
 import com.mojang.brigadier.tree.RootCommandNode;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.*;
@@ -43,6 +44,7 @@ public class PacketListener extends PacketAdapter {
                         allowedCommands.remove(matchedCommand);
                     }
                 }
+                removeBlacklist(allowedCommands, e.getPlayer());
                 matchModifier.write(0, allowedCommands.toArray(new String[allowedCommands.size()]));
             } else {
                 StructureModifier<Suggestions> matchModifier = packetContainer.getSpecificModifier(Suggestions.class);
@@ -54,10 +56,13 @@ public class PacketListener extends PacketAdapter {
                 if (split.length >= 2) {
                     allowedCommands.getList().remove(matchedCommand);
                 }
+                removeBlacklist(allowedCommands, e.getPlayer());
             }
+
                 matchModifier.write(0, allowedCommands);
             }
         } else {
+
             StructureModifier<RootCommandNode> matchModifier = packetContainer.getSpecificModifier(RootCommandNode.class);
             RootCommandNode matchedCommands = matchModifier.read(0);
             Collection map = matchedCommands.getChildren();
@@ -68,6 +73,7 @@ public class PacketListener extends PacketAdapter {
                     map.remove(matchedCommand);
                 }
             }
+            removeBlacklist(map, Arrays.asList(map.toArray(new Object[map.size()])), e.getPlayer());
         }
     }
 
@@ -76,6 +82,91 @@ public class PacketListener extends PacketAdapter {
             if (core.isLegacy()) {
                 this.incomingCommand.put(paramPacketEvent.getPlayer().getUniqueId(), paramPacketEvent.getPacket().getStrings()
                         .read(0).trim());
+            }
+        }
+    }
+
+    private void  removeBlacklist(List<String> commands, Player player) {
+        if (core.config.blacklist()) {
+            core.config.groups().forEach((key, val) -> {
+                if (player.hasPermission("plhide.group." + key.toLowerCase())) {
+                    for (String s : val.blacklist) {
+                        for (String suggestion : commands.toArray(new String[0])) {
+
+                            if (suggestion.equalsIgnoreCase(s)) {
+                                commands.remove(suggestion);
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            for (String suggestion : commands.toArray(new String[0])) {
+                for (String s : core.config.groups().keySet()) {
+                    Group group = core.config.groups().get(s);
+                    if (!group.blacklist.contains(suggestion)) {
+                        if (player.hasPermission("plhide.group." + s))
+                            commands.remove(suggestion);
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void removeBlacklist(Collection realSuggestions, Collection suggestions, Player player) {
+        if (core.config.blacklist()) {
+            core.config.groups().forEach((key, val) -> {
+                if (player.hasPermission("plhide.group." + key.toLowerCase())) {
+                    for (String s : val.blacklist) {
+                        for (Object st : suggestions) {
+                            CommandNode suggestion = (CommandNode) st;
+
+                            if (suggestion.getName().equalsIgnoreCase(s)) {
+                                realSuggestions.remove(suggestion);
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            for (Object st : suggestions) {
+                CommandNode suggestion = (CommandNode) st;
+                for (String s : core.config.groups().keySet()) {
+                    Group group = core.config.groups().get(s);
+                    if (!group.blacklist.contains(suggestion.getName())) {
+                        if (player.hasPermission("plhide.group." + s))
+                            realSuggestions.remove(suggestion);
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void removeBlacklist(Suggestions suggestions, Player player) {
+        if (core.config.blacklist()) {
+            core.config.groups().forEach((key, val) -> {
+                if (player.hasPermission("plhide.group." + key.toLowerCase())) {
+                    for (String s : val.blacklist) {
+                        for (Suggestion suggestion : suggestions.getList().toArray(new Suggestion[0])) {
+                            if (suggestion.getText().equalsIgnoreCase(s)) {
+                                suggestions.getList().remove(suggestion);
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            for (Suggestion suggestion : suggestions.getList().toArray(new Suggestion[0])) {
+                for (String s : core.config.groups().keySet()) {
+                    Group group = core.config.groups().get(s);
+                    if (!group.blacklist.contains(suggestion.getText())) {
+                        if (player.hasPermission("plhide.group." + s))
+                        suggestions.getList().remove(suggestion);
+                    }
+                }
+
             }
         }
     }
