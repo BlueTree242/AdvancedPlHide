8208eba2301bf2cec208a9bce07198d851d17074
BlueTree242 <yahiamsm2@gmail.com>
2021-12-17 16:58:47 +0200

    add check legacy

--- bukkit/src/main/java/tk/bluetree242/advancedplhide/bukkit/PacketListener.java
+++ bukkit/src/main/java/tk/bluetree242/advancedplhide/bukkit/PacketListener.java
@@ -10,10 +10,13 @@ import tk.bluetree242.advancedplhide.CompleterModifier;
 import tk.bluetree242.advancedplhide.bukkit.impl.StringCommandCompleterList;
 import tk.bluetree242.advancedplhide.bukkit.impl.SuggestionCommandCompleterList;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class PacketListener extends PacketAdapter {
 
     private AdvancedPlHideBukkit core;
-
+    private final HashMap<UUID, String> commandsWaiting = new HashMap<>();
     public PacketListener(AdvancedPlHideBukkit core) {
         super(core, ListenerPriority.NORMAL, PacketType.Play.Server.TAB_COMPLETE, PacketType.Play.Client.TAB_COMPLETE, PacketType.Play.Server.COMMANDS);
         this.core = core;
@@ -40,12 +43,20 @@ public class PacketListener extends PacketAdapter {
             StructureModifier<String[]> matchModifier = e.getPacket().getSpecificModifier(String[].class);
             String[] suggestionsOrigin = matchModifier.read(0);
             StringCommandCompleterList suggestions = new StringCommandCompleterList(suggestionsOrigin);
-            CompleterModifier.removePluginPrefix(suggestions);
+            String str = this.commandsWaiting.get(e.getPlayer().getUniqueId());
+            if (!str.contains(" ") && str.startsWith("/")) {
+                CompleterModifier.removePluginPrefix(suggestions);
+            }
             matchModifier.write(0, suggestions.export());
         }
     }
 
     public void onPacketReceiving(PacketEvent e) {
-
+        if (e.getPacketType() == PacketType.Play.Client.TAB_COMPLETE) {
+            if (core.isLegacy()) {
+                this.commandsWaiting.put(e.getPlayer().getUniqueId(), e.getPacket().getStrings()
+                        .read(0).trim());
+            }
+        }
     }
 }
