b200f29673b0c7b39e0577addd8c3fbbb686602d
BlueTree242 <yahiamsm2@gmail.com>
2025-07-24 21:52:25 +0300

    Update domain in startup message

--- core/src/main/java/dev/bluetree242/advancedplhide/utils/Constants.java
+++ core/src/main/java/dev/bluetree242/advancedplhide/utils/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
                 "|   &cRunning on: &r" + PlatformPlugin.get().getType().getName() + "\n&r" +
                 "| &cSupport:\n&r" +
                 "|   &cGithub: &rhttps://github.com/BlueTree242/AdvancedPlHide/issues\n" +
-                "|   &cDiscord: &rhttps://advancedplhide.ml/support\n" +
+                "|   &cDiscord: &rhttps://advancedplhide.bluetree242.dev/support\n" +
                 "[]================================[]";
     }
 }
